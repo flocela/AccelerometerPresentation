e6b8be7bacc6844297989ac1708479138e43b8c8
Flo B Jones <flocela@gmail.com>
2016-06-26 19:09:17 -0500

    Portrait mode is the only configuration allowed per manifest. No title bar requested in MainActivity onCreate method.

--- SteelBalls/app/src/main/java/flobee/steelballs/MainActivity.java
+++ SteelBalls/app/src/main/java/flobee/steelballs/MainActivity.java
@@ -2,6 +2,7 @@ package flobee.steelballs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
     simView = new SimulationView(this);
     setContentView(simView);
   }
